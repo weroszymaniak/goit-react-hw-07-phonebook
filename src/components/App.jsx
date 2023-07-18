a420ebba4a174abf5e319ee3fd7bd77bacf6a35b
weroszymaniak <weronika.szymaniak@hotmail.com>
2023-07-18 19:31:28 +0200

    change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,7 @@ import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 
-// import { getContacts } from 'redux/selectors';
-// import { useSelector } from 'react-redux';
-
 const App = () => {
-  // const contacts = useSelector(getContacts);
-
   return (
     <div>
       <h1>Phonebook</h1>
